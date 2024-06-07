a76cf7b2ff482068ff54c9a6764dc359823cb61f
jovannajdovski <j.najdovski@gmail.com>
2024-06-08 00:06:06 +0200

    Rework on backward style rule

---
+++ back/model/src/main/java/com/ftn/sbnz/model/models/SimilarPlayer.java
@@ -0,0 +1,15 @@
+package com.ftn.sbnz.model.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SimilarPlayer {
+    private String name;
+    private Integer injuredPlayerPoints;
+}
